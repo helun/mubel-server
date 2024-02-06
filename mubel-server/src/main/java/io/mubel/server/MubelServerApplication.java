8878697239ab1036c8741546c535f5c71dc74bc0
helun <henrikbd@hey.com>
2024-02-06 20:39:19 +0100

    improve server start up time

--- mubel-server/src/main/java/io/mubel/server/MubelServerApplication.java
+++ mubel-server/src/main/java/io/mubel/server/MubelServerApplication.java
@@ -2,9 +2,22 @@ package io.mubel.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.batch.BatchAutoConfiguration;
+import org.springframework.boot.autoconfigure.couchbase.CouchbaseAutoConfiguration;
+import org.springframework.boot.autoconfigure.graphql.GraphQlAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jms.JmsAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
 
-@SpringBootApplication
+@SpringBootApplication(
+        scanBasePackages = {"io.mubel"},
+        exclude = {
+                DataSourceAutoConfiguration.class,
+                JmsAutoConfiguration.class,
+                GraphQlAutoConfiguration.class,
+                CouchbaseAutoConfiguration.class,
+                BatchAutoConfiguration.class
+        })
 @EnableCaching
 public class MubelServerApplication {
 
