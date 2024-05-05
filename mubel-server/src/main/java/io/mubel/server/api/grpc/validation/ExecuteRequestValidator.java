b66df15caa016d8f16de2d9230fada490fdd092d
helun <henrikbd@hey.com>
2024-05-05 12:35:01 +0200

    fix validation of ExecuteRequest

--- mubel-server/src/main/java/io/mubel/server/api/grpc/validation/ExecuteRequestValidator.java
+++ mubel-server/src/main/java/io/mubel/server/api/grpc/validation/ExecuteRequestValidator.java
@@ -1,15 +1,20 @@
 package io.mubel.server.api.grpc.validation;
 
 import am.ik.yavi.builder.ValidatorBuilder;
-import am.ik.yavi.constraint.CharSequenceConstraint;
 import am.ik.yavi.core.ConstraintViolations;
 import am.ik.yavi.core.Validator;
 import io.mubel.api.grpc.v1.events.ExecuteRequest;
+import io.mubel.api.grpc.v1.events.Operation;
+
+import static io.mubel.server.api.grpc.validation.CommonConstraints.esid;
 
 public class ExecuteRequestValidator {
 
+    private static final Validator<Operation> OPERATION_VALIDATOR = OperationValidator.OPERATION_VALIDATOR;
+
     private static final Validator<ExecuteRequest> VALIDATOR = ValidatorBuilder.<ExecuteRequest>of()
-            .constraint(ExecuteRequest::getEsid, "esid", CharSequenceConstraint::notBlank)
+            .constraint(ExecuteRequest::getEsid, "esid", esid())
+            .forEach(ExecuteRequest::getOperationList, "operation", OPERATION_VALIDATOR)
             .build();
 
     public static ConstraintViolations validate(ExecuteRequest request) {
--- mubel-server/src/test/java/io/mubel/server/api/grpc/validation/ExecuteRequestValidatorTest.java
+++ mubel-server/src/test/java/io/mubel/server/api/grpc/validation/ExecuteRequestValidatorTest.java
@@ -1,6 +1,9 @@
 package io.mubel.server.api.grpc.validation;
 
+import io.mubel.api.grpc.v1.events.AppendOperation;
 import io.mubel.api.grpc.v1.events.ExecuteRequest;
+import io.mubel.api.grpc.v1.events.Operation;
+import io.mubel.server.Fixtures;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -11,6 +14,10 @@ class ExecuteRequestValidatorTest {
     void valid_request() {
         var request = ExecuteRequest.newBuilder()
                 .setEsid("esid")
+                .addOperation(Operation.newBuilder()
+                        .setAppend(AppendOperation.newBuilder()
+                                .addEvent(Fixtures.eventInput(0)))
+                )
                 .build();
         assertValid(request);
     }
@@ -19,6 +26,21 @@ class ExecuteRequestValidatorTest {
     void invalid_esid_does_not_validate() {
         var request = ExecuteRequest.newBuilder()
                 .setEsid("")
+                .build();
+        assertInvalid(request);
+    }
+
+    @Test
+    void invalid_event_does_not_validate() {
+        var request = ExecuteRequest.newBuilder()
+                .setEsid("esid")
+                .addOperation(Operation.newBuilder()
+                        .setAppend(AppendOperation.newBuilder()
+                                .addEvent(Fixtures.eventInput(0)
+                                        .toBuilder()
+                                        .setId("not-uuid")
+                                        .build()))
+                )
                 .build();
         assertInvalid(request);
     }
