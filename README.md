7421d28ecddde7e709f179a11103198447eb034e
helun <henrikbd@hey.com>
2025-02-10 20:46:11 +0100

    AGPL-3 License

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # mubel-server
+
+## Work in Progress
+
+This library is under active development and is not yet ready for use.
+
+## Description
+
+The mubel-server is the server component of a collection of libraries aimed at simplifying the development of
+event-sourcing applications.
+Event sourcing is a powerful architectural pattern used to capture all changes to an application state as a
+sequence of events.
--- mubel-provider-jdbc/src/main/java/io/mubel/provider/jdbc/eventstore/JdbcReplayService.java
+++ mubel-provider-jdbc/src/main/java/io/mubel/provider/jdbc/eventstore/JdbcReplayService.java
@@ -1,3 +1,20 @@
+/*
+ * mubel-provider-jdbc - mubel-provider-jdbc
+ * Copyright © 2025 Henrik Barratt-Due (henrikbd@hey.com)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package io.mubel.provider.jdbc.eventstore;
 
 import io.mubel.api.grpc.v1.events.EventData;
@@ -13,7 +30,6 @@ import java.util.concurrent.atomic.AtomicLong;
 
 public class JdbcReplayService implements ReplayService {
 
-    private static final int BATCH_SIZE = 256;
     private final Jdbi jdbi;
     private final EventStoreStatements statements;
     private final Scheduler scheduler;
@@ -44,7 +60,6 @@ public class JdbcReplayService implements ReplayService {
         sink.onCancel(() -> isCancelled.set(true));
         sink.onRequest(n -> jdbi.useHandle(handle -> {
             try {
-                var limit = Math.min(n, BATCH_SIZE);
                 handle.createQuery(statements.pagedReplaySql())
                         .bind(0, lastSequenceNo.get())
                         .bind(1, n)
